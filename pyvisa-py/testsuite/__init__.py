833e5493955c04e3771e908fe9457c3136fa1ba5
Hernan Grecco <hernan.grecco@gmail.com>
2014-12-08 22:24:06 -0300

    Started test suite

---
+++ pyvisa-py/testsuite/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+import os
+
+from pyvisa.compat import unittest
+
+
+def testsuite():
+    """A testsuite that has all the pyvisa tests.
+    """
+    return unittest.TestLoader().discover(os.path.dirname(__file__))
+
+
+def main():
+    """Runs the testsuite as command line application.
+    """
+    try:
+        unittest.main()
+    except Exception as e:
+        print('Error: %s' % e)
+
+
+def run():
+    """Run all tests.
+
+    :return: a :class:`unittest.TestResult` object
+    """
+    test_runner = unittest.TextTestRunner()
+    return test_runner.run(testsuite())
+
---
+++ pyvisa-py/testsuite/test_serial.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+from pyvisa.testsuite import BaseTestCase
+
+
+
+
+pyvisapy = __import__('pyvisa-py')
+SerialSession = pyvisapy.serial.SerialSession
+
+
+class Test():
+    pass
+
