07fb811ffd009fc69d7409eff94791ce18f1893b
Thomas Kopp <20.kopp@gmail.com>
2015-02-19 20:35:03 +0100

    Common base for usb tmc and usb raw

--- pyvisa-py/usb.py
+++ pyvisa-py/usb.py
@@ -16,6 +16,8 @@ from pyvisa import constants, attributes
 
 from .sessions import Session, UnknownAttribute
 
+import abc
+
 try:
     import usb
     from .protocols import usbtmc, usbutil
@@ -36,14 +38,108 @@ from . import common
 StatusCode = constants.StatusCode
 SUCCESS = StatusCode.success
 
-
-@Session.register(constants.InterfaceType.usb, 'INSTR')
 class USBSession(Session):
-    """Base class for drivers that communicate with instruments
+    """Base class for drivers that communicate with usb devices
     via usb port using pyUSB
     """
 
     timeout = 2000
+
+    @abc.abstractmethod
+    def list_resources():
+        """Return list of resources for this type of USB device"""
+
+    @classmethod
+    def get_low_level_info(cls):
+        try:
+            ver = usb.__version__
+        except AttributeError:
+            ver = 'N/A'
+
+        try:
+            # noinspection PyProtectedMember
+            backend = usb.core.find()._ctx.backend.__class__.__module__.split('.')[-1]
+        except:
+            backend = 'N/A'
+
+        return 'via PyUSB (%s). Backend: %s' % (ver, backend)
+
+    def read(self, count):
+        """Reads data from device or interface synchronously.
+
+        Corresponds to viRead function of the VISA library.
+
+        :param count: Number of bytes to be read.
+        :return: data read, return value of the library call.
+        :rtype: (bytes, VISAStatus)
+        """
+
+        supress_end_en, _ = self.get_attribute(constants.VI_ATTR_SUPPRESS_END_EN)
+
+        if supress_end_en:
+            raise ValueError('VI_ATTR_SUPPRESS_END_EN == True is currently unsupported by pyvisa-py')
+
+        term_char, _ = self.get_attribute(constants.VI_ATTR_TERMCHAR)
+        term_char_en, _ = self.get_attribute(constants.VI_ATTR_TERMCHAR_EN)
+
+        return self._read(lambda: self.interface.read(1),
+                          count,
+                          lambda current: False,
+                          supress_end_en,
+                          term_char,
+                          term_char_en,
+                          usb.USBError)
+
+    def write(self, data):
+        """Writes data to device or interface synchronously.
+
+        Corresponds to viWrite function of the VISA library.
+
+        :param data: data to be written.
+        :type data: bytes
+        :return: Number of bytes actually transferred, return value of the library call.
+        :rtype: (int, VISAStatus)
+        """
+
+        send_end, _ = self.get_attribute(constants.VI_ATTR_SEND_END_EN)
+
+        count = self.interface.write(data)
+
+        return count, SUCCESS
+
+    def close(self):
+        self.interface.close()
+
+    def _get_attribute(self, attribute):
+        """Get the value for a given VISA attribute for this session.
+
+        Use to implement custom logic for attributes.
+
+        :param attribute: Resource attribute for which the state query is made
+        :return: The state of the queried attribute for a specified resource, return value of the library call.
+        :rtype: (unicode | str | list | int, VISAStatus)
+        """
+
+        raise UnknownAttribute(attribute)
+
+    def _set_attribute(self, attribute, attribute_state):
+        """Sets the state of an attribute.
+
+        Corresponds to viSetAttribute function of the VISA library.
+
+        :param attribute: Attribute for which the state is to be modified. (Attributes.*)
+        :param attribute_state: The state of the attribute to be set for the specified object.
+        :return: return value of the library call.
+        :rtype: VISAStatus
+        """
+
+        raise UnknownAttribute(attribute)
+
+@Session.register(constants.InterfaceType.usb, 'INSTR')
+class USBInstrSession(USBSession):
+    """Base class for drivers that communicate with instruments
+    via usb port using pyUSB
+    """
 
     @staticmethod
     def list_resources():
@@ -63,21 +159,6 @@ class USBSession(Session):
                                   usb_interface_number=intfc))
         return out
 
-    @classmethod
-    def get_low_level_info(cls):
-        try:
-            ver = usb.__version__
-        except AttributeError:
-            ver = 'N/A'
-
-        try:
-            # noinspection PyProtectedMember
-            backend = usb.core.find()._ctx.backend.__class__.__module__.split('.')[-1]
-        except:
-            backend = 'N/A'
-
-        return 'via PyUSB (%s). Backend: %s' % (ver, backend)
-
     def after_parsing(self):
         self.interface = usbtmc.USBTMC(int(self.parsed['manufacturer_id'], 0),
                                        int(self.parsed['model_code'], 0),
@@ -87,84 +168,12 @@ class USBSession(Session):
             attribute = getattr(constants, 'VI_ATTR_' + name)
             self.attrs[attribute] = attributes.AttributesByID[attribute].default
 
-    def read(self, count):
-        """Reads data from device or interface synchronously.
-
-        Corresponds to viRead function of the VISA library.
-
-        :param count: Number of bytes to be read.
-        :return: data read, return value of the library call.
-        :rtype: (bytes, VISAStatus)
-        """
-
-        supress_end_en, _ = self.get_attribute(constants.VI_ATTR_SUPPRESS_END_EN)
-
-        if supress_end_en:
-            raise ValueError('VI_ATTR_SUPPRESS_END_EN == True is currently unsupported by pyvisa-py')
-
-        term_char, _ = self.get_attribute(constants.VI_ATTR_TERMCHAR)
-        term_char_en, _ = self.get_attribute(constants.VI_ATTR_TERMCHAR_EN)
-
-        return self._read(lambda: self.interface.read(1),
-                          count,
-                          lambda current: False,
-                          supress_end_en,
-                          term_char,
-                          term_char_en,
-                          usb.USBError)
-
-    def write(self, data):
-        """Writes data to device or interface synchronously.
-
-        Corresponds to viWrite function of the VISA library.
-
-        :param data: data to be written.
-        :type data: bytes
-        :return: Number of bytes actually transferred, return value of the library call.
-        :rtype: (int, VISAStatus)
-        """
-
-        send_end, _ = self.get_attribute(constants.VI_ATTR_SEND_END_EN)
-
-        count = self.interface.write(data)
-
-        return count, SUCCESS
-
-    def close(self):
-        self.interface.close()
-
-    def _get_attribute(self, attribute):
-        """Get the value for a given VISA attribute for this session.
-
-        Use to implement custom logic for attributes.
-
-        :param attribute: Resource attribute for which the state query is made
-        :return: The state of the queried attribute for a specified resource, return value of the library call.
-        :rtype: (unicode | str | list | int, VISAStatus)
-        """
-
-        raise UnknownAttribute(attribute)
-
-    def _set_attribute(self, attribute, attribute_state):
-        """Sets the state of an attribute.
-
-        Corresponds to viSetAttribute function of the VISA library.
-
-        :param attribute: Attribute for which the state is to be modified. (Attributes.*)
-        :param attribute_state: The state of the attribute to be set for the specified object.
-        :return: return value of the library call.
-        :rtype: VISAStatus
-        """
-
-        raise UnknownAttribute(attribute)
 
 @Session.register(constants.InterfaceType.usb, 'RAW')
-class USBRawSession(Session):
+class USBRawSession(USBSession):
     """Base class for drivers that communicate with usb raw devices
     via usb port using pyUSB
     """
-
-    timeout = 2000
 
     @staticmethod
     def list_resources():
@@ -184,21 +193,6 @@ class USBRawSession(Session):
                                   usb_interface_number=intfc))
         return out
 
-    @classmethod
-    def get_low_level_info(cls):
-        try:
-            ver = usb.__version__
-        except AttributeError:
-            ver = 'N/A'
-
-        try:
-            # noinspection PyProtectedMember
-            backend = usb.core.find()._ctx.backend.__class__.__module__.split('.')[-1]
-        except:
-            backend = 'N/A'
-
-        return 'via PyUSB (%s). Backend: %s' % (ver, backend)
-
     def after_parsing(self):
         self.interface = usbraw.USBRawDevice(int(self.parsed['manufacturer_id'], 0),
                                              int(self.parsed['model_code'], 0),
@@ -207,74 +201,3 @@ class USBRawSession(Session):
         for name in 'SEND_END_EN,TERMCHAR,TERMCHAR_EN'.split(','):
             attribute = getattr(constants, 'VI_ATTR_' + name)
             self.attrs[attribute] = attributes.AttributesByID[attribute].default
-
-    def read(self, count):
-        """Reads data from device or interface synchronously.
-
-        Corresponds to viRead function of the VISA library.
-
-        :param count: Number of bytes to be read.
-        :return: data read, return value of the library call.
-        :rtype: (bytes, VISAStatus)
-        """
-
-        supress_end_en, _ = self.get_attribute(constants.VI_ATTR_SUPPRESS_END_EN)
-
-        if supress_end_en:
-            raise ValueError('VI_ATTR_SUPPRESS_END_EN == True is currently unsupported by pyvisa-py')
-
-        term_char, _ = self.get_attribute(constants.VI_ATTR_TERMCHAR)
-        term_char_en, _ = self.get_attribute(constants.VI_ATTR_TERMCHAR_EN)
-
-        return self._read(lambda: self.interface.read(1),
-                          count,
-                          lambda current: False,
-                          supress_end_en,
-                          term_char,
-                          term_char_en,
-                          usb.USBError)
-
-    def write(self, data):
-        """Writes data to device or interface synchronously.
-
-        Corresponds to viWrite function of the VISA library.
-
-        :param data: data to be written.
-        :type data: bytes
-        :return: Number of bytes actually transferred, return value of the library call.
-        :rtype: (int, VISAStatus)
-        """
-
-        send_end, _ = self.get_attribute(constants.VI_ATTR_SEND_END_EN)
-
-        count = self.interface.write(data)
-
-        return count, SUCCESS
-
-    def close(self):
-        self.interface.close()
-
-    def _get_attribute(self, attribute):
-        """Get the value for a given VISA attribute for this session.
-
-        Use to implement custom logic for attributes.
-
-        :param attribute: Resource attribute for which the state query is made
-        :return: The state of the queried attribute for a specified resource, return value of the library call.
-        :rtype: (unicode | str | list | int, VISAStatus)
-        """
-
-        raise UnknownAttribute(attribute)
-
-    def _set_attribute(self, attribute, attribute_state):
-        """Sets the state of an attribute.
-
-        Corresponds to viSetAttribute function of the VISA library.
-
-        :param attribute: Attribute for which the state is to be modified. (Attributes.*)
-        :param attribute_state: The state of the attribute to be set for the specified object.
-        :return: return value of the library call.
-        :rtype: VISAStatus
-        """
-
-        raise UnknownAttribute(attribute)
