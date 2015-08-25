d4ca118748a44ff2aff87a9ec75591619aa9adcf
Hernan Grecco <hernan.grecco@gmail.com>
2015-08-24 23:53:50 -0300

    Requires PyVISA >= 1.8

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ long_description = '\n\n'.join([read('README'),
 
 __doc__ = long_description
 
-requirements = ['pyvisa>=1.7']
+requirements = ['pyvisa>=1.8']
 
 if sys.version_info < (2, 7):
     requirements.append('importlib')
