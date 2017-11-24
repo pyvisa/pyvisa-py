72d85272e62b24ae370532a3b06e3993bf88c843
skrchnavy <svetozar.krchnavy@anritsu.com>
2017-11-24 12:43:10 +0100

    pyserial renamed writeTimeout to write_timeout in version 3.0

--- pyvisa-py/serial.py
+++ pyvisa-py/serial.py
@@ -65,7 +65,7 @@ class SerialSession(Session):
         else:
             cls = Serial
 
-        self.interface = cls(port=self.parsed.board, timeout=2000, writeTimeout=2000)
+        self.interface = cls(port=self.parsed.board, timeout=2000, write_timeout=2000)
 
         for name in ('ASRL_END_IN', 'ASRL_END_OUT', 'SEND_END_EN', 'TERMCHAR',
                     'TERMCHAR_EN', 'SUPPRESS_END_EN'):
@@ -93,7 +93,7 @@ class SerialSession(Session):
             value = value / 1000.
 
         self.interface.timeout = value
-        self.interface.writeTimeout = value
+        self.interface.write_timeout = value
 
     def close(self):
         self.interface.close()
