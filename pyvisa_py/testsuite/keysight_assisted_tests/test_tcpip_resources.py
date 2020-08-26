b5ade0a3ed859ec7b34ba6e8df5a714019676f63
MatthieuDartiailh <marul@laposte.net>
2020-08-26 19:31:25 -0400

    tests: run black

--- pyvisa_py/testsuite/keysight_assisted_tests/test_tcpip_resources.py
+++ pyvisa_py/testsuite/keysight_assisted_tests/test_tcpip_resources.py
@@ -12,9 +12,7 @@ from pyvisa.testsuite.keysight_assisted_tests.test_tcpip_resources import (
 
 @require_virtual_instr
 class TestTCPIPInstr(TCPIPInstrBaseTest):
-    """Test pyvisa-py against a TCPIP INSTR resource.
-
-    """
+    """Test pyvisa-py against a TCPIP INSTR resource."""
 
     #: Type of resource being tested in this test case.
     #: See RESOURCE_ADDRESSES in the __init__.py file of this package for
@@ -90,9 +88,7 @@ class TestTCPIPInstr(TCPIPInstrBaseTest):
 
 @require_virtual_instr
 class TestTCPIPSocket(TCPIPSocketBaseTest):
-    """Test pyvisa-py against a TCPIP SOCKET resource.
-
-    """
+    """Test pyvisa-py against a TCPIP SOCKET resource."""
 
     #: Type of resource being tested in this test case.
     #: See RESOURCE_ADDRESSES in the __init__.py file of this package for
