a9221593a2b254ed55a05c7525240eee16c6e46d
Douglas Thor <dougthor42@users.noreply.github.com>
2023-10-19 08:52:19 -0700

    Update iter_bytes to be the same as what's used in `pyvisa-sim` (#389)
    * Temp update pre-commit
    
    * Add common.py and tests
    
    * Update serial.iter_bytes to use the new function.
    
    * Udpate SerialSession.write to use new iter_bytes code
    
    * Update changelog
    
    * Remove iter_bytes from serial.py; update changelog
    
    * Revert "Temp update pre-commit"
    
    This reverts commit c45e050a902237cb17a12ee835f947af4ff931cb.
    
    * Fix lint issues

--- pyvisa_py/serial.py
+++ pyvisa_py/serial.py
@@ -7,7 +7,7 @@
 
 """
 import sys
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Tuple
 
 from pyvisa import attributes, constants, logger, rname
 from pyvisa.constants import (
@@ -32,24 +32,6 @@ except ImportError as e:
     raise
 
 IS_WIN = sys.platform == "win32"
-
-
-def iter_bytes(data: bytes, mask: Optional[int] = None, send_end: bool = False):
-    if send_end and mask is None:
-        raise ValueError("send_end requires a valid mask.")
-
-    if mask is None:
-        for d in data:
-            yield bytes([d])
-
-    else:
-        for d in data[:-1]:
-            yield bytes([d & ~mask])
-
-        if send_end:
-            yield bytes([data[-1] | ~mask])
-        else:
-            yield bytes([data[-1] & ~mask])
 
 
 def to_state(boolean_input: bool) -> constants.LineState:
@@ -184,20 +166,20 @@ class SerialSession(Session):
 
         """
         logger.debug("Serial.write %r" % data)
-        end_out, _ = self.get_attribute(ResourceAttribute.asrl_end_out)
         send_end, _ = self.get_attribute(ResourceAttribute.send_end_enabled)
+        end_out, _ = self.get_attribute(ResourceAttribute.asrl_end_out)
+        data_bits, _ = self.get_attribute(constants.ResourceAttribute.asrl_data_bits)
 
-        if end_out in (SerialTermination.none, SerialTermination.termination_break):
+        if end_out == SerialTermination.none:
             pass
         elif end_out == SerialTermination.last_bit:
-            last_bit, _ = self.get_attribute(ResourceAttribute.asrl_data_bits)
-            mask = 1 << (last_bit - 1)
-            data = bytes(iter_bytes(data, mask, send_end))
-
+            data = b"".join(common.iter_bytes(data, data_bits, send_end))
         elif end_out == SerialTermination.termination_char:
             term_char, _ = self.get_attribute(ResourceAttribute.termchar)
+            data = b"".join(common.iter_bytes(data, data_bits, send_end=None))
             data = data + common.int_to_byte(term_char)
-
+        elif end_out == SerialTermination.termination_break:
+            data = b"".join(common.iter_bytes(data, data_bits, send_end=None))
         else:
             raise ValueError("Unknown value for VI_ATTR_ASRL_END_OUT: %s" % end_out)
 
