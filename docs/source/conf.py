e2fc529f7a930211ecd9b3da7e7208e37a35531e
MatthieuDartiailh <m.dartiailh@gmail.com>
2024-10-28 15:33:03 +0100

    fix docs building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,6 @@
 # serve to show the default.
 
 import datetime
-import os
-import sys
 from importlib.metadata import version as get_version
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -99,19 +97,6 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "sphinx_rtd_theme"
-
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    try:
-        import sphinx_rtd_theme
-    except ImportError:
-        print(
-            "\n\nTheme not found. Please install Sphinx Read The Docs Themes using:\n\n"
-            "   pip install sphinx_rtd_theme\n"
-        )
-        sys.exit(1)
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
