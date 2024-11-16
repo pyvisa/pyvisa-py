cc66f1e1623e01556fca634b04d08326e685d4f5
user <>
2024-11-16 04:00:27 +0000

    Write long data via USBTMC correctly
    Previously the variable holding the data would get overwritten by
    mistake when it could not fit in a single packet.

--- pyvisa_py/protocols/usbtmc.py
+++ pyvisa_py/protocols/usbtmc.py
@@ -448,9 +448,9 @@ class USBTMC(USBRaw):
             self._btag = (self._btag % 255) + 1
 
             eom = end >= size
-            data = BulkOutMessage.build_array(self._btag, eom, data[begin:end])
+            chunk = BulkOutMessage.build_array(self._btag, eom, data[begin:end])
 
-            bytes_sent += raw_write(data)
+            bytes_sent += raw_write(chunk)
 
         return size
 
