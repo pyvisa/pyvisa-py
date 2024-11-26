fde8f5a542225a324577b7a6959c69dbc2b8593a
MatthieuDartiailh <m.dartiailh@gmail.com>
2024-11-26 09:19:14 +0100

    fix linting issues

--- pyvisa_py/__init__.py
+++ pyvisa_py/__init__.py
@@ -16,7 +16,7 @@ except PackageNotFoundError:
     # package is not installed
     pass
 
-# noqa: we need to import so that __init_subclass__() is executed once
+# We need to import all attributes so that __init_subclass__() is executed once
 from . import attributes  # noqa: F401
 from .highlevel import PyVisaLibrary
 
