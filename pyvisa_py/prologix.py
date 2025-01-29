5578e84c58a8bacc4a938903fe010bed48f4318d
Bob McNamara <rsm@rivosinc.com>
2025-01-29 12:27:33 -0800

    added mutex to protect gpib_addr changes; added comments about discarding stale read data

--- pyvisa_py/prologix.py
+++ pyvisa_py/prologix.py
@@ -5,6 +5,7 @@ Implements the interface and instrument classes for Prologix-style devices.
 import select
 import socket
 import sys
+import threading
 from typing import Any
 
 from pyvisa import attributes, constants, errors, logger, rname
@@ -66,6 +67,8 @@ class _PrologixIntfcSession(Session):  # pylint: disable=W0223
         BOARDS[self.parsed.board] = self
         self._gpib_addr = ""
 
+        self.intfc_lock = threading.Lock()
+
     def close(self) -> StatusCode:
         BOARDS.pop(self.parsed.board)
         return super().close()  # type: ignore[safe-super]
@@ -83,14 +86,14 @@ class _PrologixIntfcSession(Session):  # pylint: disable=W0223
             self.write_oob(f"++addr {addr}\n".encode())
             self._gpib_addr = addr
 
-    def write_oob(self, data: bytes) -> Tuple[int, StatusCode]:
+    def write_oob(self, data: bytes) -> tuple[int, StatusCode]:
         """out-of-band write (for sending "++" commands)"""
         if self.interface is None:
             raise errors.InvalidSession()
 
         return super().write(data)
 
-    def read(self, count: int) -> Tuple[bytes, StatusCode]:
+    def read(self, count: int) -> tuple[bytes, StatusCode]:
         if self.interface is None:
             raise errors.InvalidSession()
 
@@ -144,11 +147,13 @@ class PrologixTCPIPIntfcSession(_PrologixIntfcSession, TCPIPSocketSession):
             # use select to wait for write ready
             rd, _wr, _ = select.select([self.interface], [self.interface], [])
             if rd:
+                # any data that hasn't been read yet is now considered stale,
+                # and should be discarded.
                 self.clear()
         except socket.timeout:
             return 0, StatusCode.error_io
 
-        self._pending_buffer.clear()
+        self._pending_buffer.clear()  # discard any stale unread data
         self.plus_plus_read = True
         return super().write(data)
 
@@ -175,8 +180,6 @@ class PrologixASRLIntfcSession(_PrologixIntfcSession, SerialSession):
             write_timeout=self.timeout,
             baudrate=115200,
         )
-        # self.interface.write_termination = "\n"
-        # self.write_termination = "\n"
 
         for name in (
             "ASRL_END_IN",
@@ -189,7 +192,7 @@ class PrologixASRLIntfcSession(_PrologixIntfcSession, SerialSession):
             attribute = getattr(constants, "VI_ATTR_" + name)
             self.attrs[attribute] = attributes.AttributesByID[attribute].default
 
-    def write(self, data: bytes) -> Tuple[int, StatusCode]:
+    def write(self, data: bytes) -> tuple[int, StatusCode]:
         """Writes data to device or interface synchronously.
 
         Corresponds to viWrite function of the VISA library.
@@ -211,6 +214,8 @@ class PrologixASRLIntfcSession(_PrologixIntfcSession, SerialSession):
             raise errors.InvalidSession()
 
         if self.interface.inWaiting() > 0:
+            # any data that hasn't been read yet is now considered stale,
+            # and should be discarded.
             self.interface.flushInput()
         self.plus_plus_read = True
         return super().write(data)
@@ -253,19 +258,17 @@ class PrologixInstrSession(Session):
         self.interface = None
         return StatusCode.success
 
-    def read(self, count: int) -> Tuple[bytes, StatusCode]:
+    def read(self, count: int) -> tuple[bytes, StatusCode]:
         if self.interface is None or self.interface.interface is None:
             raise errors.InvalidSession()
 
-        self.interface.gpib_addr = self.gpib_addr
+        with self.interface.intfc_lock:
+            self.interface.gpib_addr = self.gpib_addr
+            return self.interface.read(count)
 
-        return self.interface.read(count)
-
-    def write(self, data: bytes) -> Tuple[int, StatusCode]:
+    def write(self, data: bytes) -> tuple[int, StatusCode]:
         if self.interface is None or self.interface.interface is None:
             raise errors.InvalidSession()
-
-        self.interface.gpib_addr = self.gpib_addr
 
         # if the calling function has appended a newline to the data,
         # we don't want it to be escaped.  remove it from the data
@@ -289,7 +292,9 @@ class PrologixInstrSession(Session):
         data = data.replace(b"\r", b"\033\r")
         data = data.replace(b"+", b"\033+")
 
-        return self.interface.write(data + last_byte)
+        with self.interface.intfc_lock:
+            self.interface.gpib_addr = self.gpib_addr
+            return self.interface.write(data + last_byte)
 
     def flush(self, mask: BufferOperation) -> StatusCode:
         if self.interface is None or self.interface.interface is None:
@@ -312,8 +317,10 @@ class PrologixInstrSession(Session):
         if self.interface is None or self.interface.interface is None:
             raise errors.InvalidSession()
 
-        self.interface.gpib_addr = self.gpib_addr
-        _, status_code = self.interface.write_oob(b"++clr\n")
+        with self.interface.intfc_lock:
+            self.interface.gpib_addr = self.gpib_addr
+            _, status_code = self.interface.write_oob(b"++clr\n")
+
         return status_code
 
     def assert_trigger(self, protocol: constants.TriggerProtocol) -> StatusCode:
@@ -336,21 +343,25 @@ class PrologixInstrSession(Session):
         if self.interface is None or self.interface.interface is None:
             raise errors.InvalidSession()
 
-        self.interface.gpib_addr = self.gpib_addr
-        _, status_code = self.interface.write_oob(b"++trg\n")
+        with self.interface.intfc_lock:
+            self.interface.gpib_addr = self.gpib_addr
+            _, status_code = self.interface.write_oob(b"++trg\n")
+
         return status_code
 
-    def read_stb(self) -> Tuple[int, StatusCode]:
+    def read_stb(self) -> tuple[int, StatusCode]:
         """Read the device status byte."""
         if self.interface is None or self.interface.interface is None:
             raise errors.InvalidSession()
 
-        self.interface.gpib_addr = self.gpib_addr
-        self.interface.write_oob(b"++spoll\n")
-        data, status_code = self.interface.read(32)
+        with self.interface.intfc_lock:
+            self.interface.gpib_addr = self.gpib_addr
+            self.interface.write_oob(b"++spoll\n")
+            data, status_code = self.interface.read(32)
+
         return (int(data), status_code)
 
-    def _get_attribute(self, attribute: ResourceAttribute) -> Tuple[Any, StatusCode]:
+    def _get_attribute(self, attribute: ResourceAttribute) -> tuple[Any, StatusCode]:
         """Get the value for a given VISA attribute for this session.
 
         Use to implement custom logic for attributes.
